aae4a89574f67492e62325d393700d307bd4b753
Harrison Verrios <harrison.v1@live.com>
2021-08-18 22:09:26 +1000

    Added in calculator to the code

--- ros-2torials/basic/turtle_pub.py
+++ ros-2torials/basic/turtle_pub.py
@@ -2,21 +2,28 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float32MultiArray
 
 class Publisher (Node):
 
     def __init__ (self):
         super().__init__('node_pub')
-        self.publisher = self.create_publisher(String, 'my_topic', 10)
+        self.publisher = self.create_publisher(Float32MultiArray, 'my_topic', 10)
         self.timer = self.create_timer(0.5, self.callback_func)
 
     def callback_func (self):
-        msg = String()
-        msg.data = input("Please enter a word: ")
+        try:
+            msg = Float32MultiArray()
+            input_a = input("Enter Number 1: ")
+            msg.data.append(float(input_a))
+            input_b = input("Eneter Number 2: ")
+            msg.data.append(float(input_b))
 
-        self.publisher.publish(msg)
-        self.get_logger().info("Publishing: %s" % msg.data)
+            self.publisher.publish(msg)
+            self.get_logger().info("Publishing: %s" % str(msg.data))
+            
+        except:
+            print("Invalid Number")
 
 def main ():
     rclpy.init()
--- ros-2torials/basic/turtle_sub.py
+++ ros-2torials/basic/turtle_sub.py
@@ -2,16 +2,19 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float32MultiArray
 
 class Subscriber (Node):
-    
+
     def __init__(self):
         super().__init__('node_sub')
-        self.subscription = self.create_subscription(String, 'my_topic', self.callback_func, 10)
+        self.subscription = self.create_subscription(Float32MultiArray, 'my_topic', self.callback_func, 10)
     
     def callback_func (self, msg):
-        self.get_logger().info("Received: %s" % msg.data)
+        val = 0
+        for num in msg.data:
+            val += num
+        self.get_logger().info("Received: %f" % val)
 
 def main ():
     rclpy.init()
