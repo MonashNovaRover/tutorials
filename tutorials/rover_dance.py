5fbe17e2201bed8350aaefc4b38711ae4f7a5d74
NikoVeee <nikogv14@gmail.com>
2022-10-06 14:23:02 +1100

    updated rover_dance.py

--- tutorials/rover_dance.py
+++ tutorials/rover_dance.py
@@ -4,12 +4,12 @@ import rclpy
 from rclpy.node import Node
 from core.msg import DriveInput
 from rclpy.duration import Duration
-from rclpy.qos import QoSProfile QoSReliabilityProfile
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 
 class DancingRobot(Node):
     def __init__(self):
         qos = QoSProfile(
-                reliability=QoSReliabilityProfile.BEST_EFFORT,
+                reliability=QoSReliabilityPolicy.BEST_EFFORT,
                 depth=1,
                 deadline=Duration(nanoseconds=2e8)
             )
@@ -17,15 +17,15 @@ class DancingRobot(Node):
         super().__init__("do_the_robot")
 
         self.publisher = self.create_publisher(DriveInput, '/control/drive_inputs', qos)
-        self.timer = self.create_timer(1, self.callback_func)
+        self.timer = self.create_timer(0.1, self.callback_func)
         self.go_forward = True
     
 
     def callback_func(self):
         if self.go_forward:
-            self.publisher.publish(DriveInput(speed=0.5, steer=0))
+            self.publisher.publish(DriveInput(speed=0.5, steer=0.))
         else:
-            self.publisher.publish(DriveInput(speed=-0.5, steer=0))
+            self.publisher.publish(DriveInput(speed=-0.5, steer=0.))
         
         self.go_forward = not self.go_forward
 
